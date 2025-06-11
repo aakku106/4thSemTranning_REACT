f65355da163cf039ed229e8f4daa185637eb8057
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-11 14:31:09 +0545

    Implement GroupList component with dynamic list rendering and update styles for improved UI

--- First_react_app/src/components/GroupList.tsx
+++ First_react_app/src/components/GroupList.tsx
@@ -1,6 +1,19 @@
 /** @format */
 
 function GroupList() {
-  return <div>GroupList</div>;
+  const list = ["cat", "dog", "rat", "cow", "chimp", "graf"];
+  return (
+    <>
+      <ul>
+        {list.map((i) => (
+          <li key={i}>{i}</li>
+        ))}
+
+        {/* 
+              map function is used differently here we cant use { } at all 
+        */}
+      </ul>
+    </>
+  );
 }
 export default GroupList;
