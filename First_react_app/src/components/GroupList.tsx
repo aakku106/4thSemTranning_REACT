72fd3367f99e543137a058665bf1278f4efd285e
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-11 15:15:25 +0545

    Refactor GroupList component: change list declaration to let, clear list initialization, and update conditional rendering for improved readability

--- First_react_app/src/components/GroupList.tsx
+++ First_react_app/src/components/GroupList.tsx
@@ -1,14 +1,18 @@
 /** @format */
 
 function GroupList() {
-  const list = ["cat", "dog", "rat", "cow", "chimp", "graf"];
+  let list = ["cat", "dog", "rat", "cow", "chimp", "graf"];
+  list = [];
+
   return (
     <>
       <ul>
+        {/* {list.length === 0 ? <p>No item in List</p> : null} */}
+        {list.length === 0 && <p>No item in List</p>}
+        {/* // this let us write a concise code */}
         {list.map((i) => (
           <li key={i}>{i}</li>
         ))}
-
         {/* 
               map function is used differently here we cant use { } at all 
         */}
