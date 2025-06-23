e0ab45ea1243e331fba2fc71999c7398fc81af15
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-23 10:58:56 +0545

    Day-1

---
+++ day1/da1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './style/style.css'
+import We from "./components/we"
+function App() {
+
+  return (
+    <>
+      <We />
+      <div>weeeeeeeee</div>
+      <h1>huuuuuuuuuuuuuuuuuu</h1>
+    </>
+  )
+}
+
+export default App
---
+++ day1/da1/src/components/We.jsx
@@ -0,0 +1,10 @@
+let We = () => {
+  return (<>
+    <div className="p-6 text-red-500 bg-red-300 text-center">
+      weeeeeeeeeee
+    </div>
+    weeeeeeeeeeeeeeeeeeeeeeeee
+  </>
+  )
+}
+export default We 
