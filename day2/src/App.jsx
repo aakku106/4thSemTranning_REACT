ec334cafe993bbac7566166f90df7d6176418b08
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-30 18:36:10 +0545

    made more structural structure

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -6,14 +6,19 @@ import { NavBar } from "./Components/navBar.jsx";
 import { Body, Greeting } from "./Components/Body.jsx";
 import { Footer } from "./Components/Footer.jsx";
 import { PlayGround } from "./Components/PlayGround1.jsx";
+import { Body2 } from "./Components/Body2.jsx";
 
 let App = () => {
   return (
-    <>
-      {/* <NavBar /> */}
-      <Body />
-      {/* <Footer /> */}
-    </>
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      {/* This is where the content expands */}
+      <main className="flex-grow">
+        {/* <Body /> */}
+        <Body2 />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
