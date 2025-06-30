10bb87d38ba7bba8240a6ed6f46fb7a05382460f
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-30 18:36:36 +0545

    added a 2nd body to remove claugher

---
+++ day2/src/Components/Body2.jsx
@@ -0,0 +1,12 @@
+import { Products } from "./Products.jsx"
+
+export let Body2 = () => {
+  return (
+    <>
+      <article className="h-160">
+        <Products />
+      </article>
+    </>
+  );
+};
+
