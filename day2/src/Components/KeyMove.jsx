b7b9cddacfe682fb1179c7442920a8eb1de218d0
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-13 14:22:02 +0545

    Final lop back and auto move

--- day2/src/Components/KeyMove.jsx
+++ day2/src/Components/KeyMove.jsx
@@ -2,49 +2,66 @@ import { useEffect, useState } from "react"
 
 export let KeyMove = () => {
   let [position, setPosition] = useState({ x: 0, y: 0 })
+  let [direction, setDirecton] = useState("")
+
   useEffect(() => {
-    function handlePress(e) {
-      let newPos = { ...position }
-      if (e.key === "ArrowUp") {
-        newPos.y -= 10;
-        setPosition(newPos);
-        console.log(position);
-        if (position.y === -240) {
-          newPos.y = 240
-        }
-      } else if (e.key === "ArrowDown") {
-        newPos.y += 10
-        setPosition(newPos);
-        if (position.y === 240) {
-          newPos.y = -240
-        }
-      }
-      else if (e.key === "ArrowRight") {
-        newPos.x += 10;
-        setPosition(newPos)
-        console.log(position);
-        if (position.x === 240) {
-          newPos.x = -240
-        }
-      }
-      else if (e.key === "ArrowLeft") {
-        newPos.x -= 10
-        setPosition(newPos)
-        console.log(position);
-        if (position.x === -240) {
-          newPos.x = 240
-        }
+    let newPos = { ...position }
+    let up = () => {
+      newPos.y -= 10;
+      setPosition(newPos);
+      console.log(position);
+      setDirecton("ArrowUp")
+      if (position.y === -240) {
+        newPos.y = 240
       }
     }
+    let down = () => {
+      newPos.y += 10
+      setPosition(newPos);
+      setDirecton("ArrowDown")
+      if (position.y === 240) {
+        newPos.y = -240
+      }
+    }
+    let right = () => {
+      newPos.x += 10;
+      setPosition(newPos)
+      console.log(position);
+      setDirecton("ArrowRight")
+      if (position.x === 240) {
+        newPos.x = -240
+      }
+    }
+    let left = () => {
+      newPos.x -= 10
+      setPosition(newPos)
+      console.log(position);
+      setDirecton("ArrowLeft")
+      if (position.x === -240) {
+        newPos.x = 240
+      }
 
-    let autoMove = setInterval(() => {
-
-      handlePress
-
-    }, 600)
-
-
+    }
+    function handlePress(e) {
+      if (e.key === "ArrowUp") {
+        up()
+      } else if (e.key === "ArrowDown") {
+        down()
+      }
+      else if (e.key === "ArrowRight") {
+        right()
+      }
+      else if (e.key === "ArrowLeft") {
+        left()
+      }
+    }
     window.addEventListener("keydown", handlePress);
+    let autoMove = setInterval(() => {
+      if (direction === "ArrowUp") { up() }
+      if (direction === "ArrowDown") { down() }
+      if (direction === "ArrowRight") { right() }
+      if (direction === "ArrowLeft") { left() }
+    }, 100)
     return () => {
       window.removeEventListener("keydown", handlePress)
       clearInterval(autoMove);
