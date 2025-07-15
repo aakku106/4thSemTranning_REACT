329693b844f9bc6e15b0b3b8af72d8209c59a6da
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-15 08:50:13 +0545

    Using map to make snake

--- day2/src/Components/SnakeGame/Snake.jsx
+++ day2/src/Components/SnakeGame/Snake.jsx
@@ -1,6 +1,19 @@
-export let Snake = ({ position, count, direction }) => {
+export let Snake = ({ position, count, direction, snake }) => {
   return (
     <>
+      {
+        snake.map((val, index) => {
+          <div
+            className="snake"
+            key={index}
+            style={{
+              width: `${count}px`,
+              top: `${position.y}px`,
+              left: `${position.x}px`,
+              transform: `rotate(${direction[0] === "ArrowDown" ? "-90deg" : direction[0] === "ArrowUp" ? "90deg" : direction[0] === "ArrowLeft" ? "0deg" : "180deg"})`
+            }} ></div>
+        })
+      }
       <div
         className="snake"
         style={{
