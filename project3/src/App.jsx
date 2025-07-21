601801a26ee579c617cceabe9bd8cc5b1e20cf73
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-21 13:30:09 +0545

    usecase of useref in react

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -2,17 +2,32 @@ import { useEffect, useRef, useState } from "react"
 
 function App() {
   let [text, setText] = useState("")
-  let [count, setCount] = useState(0)
+  //  let [count, setCount] = useState(0)
+  //  useEffect(() => {
+  //    setCount(prev => prev + 1)
+  //  }, [text])
+
+
+  let inputref = useRef(null)
+  let count = useRef(0);
+  let prevValOfTxt = useRef('')
   useEffect(() => {
-    setCount(prev => prev + 1)
+    count.current = count.current + 1
+    prevValOfTxt.current = text
   }, [text])
+  function handleFocus() {
+    console.log(inputref);
+    inputref.current.focus();
+  }
 
   return (
     <>
       <div>
-        <input type="text" value={text} onChange={(e) => { setText(e.target.value) }} />
+        <input ref={inputref} type="text" value={text} onChange={(e) => { setText(e.target.value) }} />
+        <button onClick={handleFocus} >Focus</button>
         <div>You typed {text}</div>
-        <div>tou rendered {count} times</div>
+        <div>tou rendered {count.current} {console.log(count.current)} times</div>
+        <div>prev val: {prevValOfTxt.current}</div>const
       </div>
     </>
   )
