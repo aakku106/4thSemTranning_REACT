0d7660753aaa8b75f49d3e02457e1787ab9ae071
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-21 14:18:30 +0545

    .

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -7,8 +7,11 @@ function App() {
   //    setCount(prev => prev + 1)
   //  }, [text])
 
+  let [visible, setVisible] = useState(false)
+
 
   let inputref = useRef(null)
+  let input = useRef(null)
   let count = useRef(0);
   let prevValOfTxt = useRef('')
   useEffect(() => {
@@ -16,18 +19,52 @@ function App() {
     prevValOfTxt.current = text
   }, [text])
   function handleFocus() {
-    console.log(inputref);
     inputref.current.focus();
   }
+  function unFocus() {
+    inputref.current.blur()
+  }
+
+  useEffect(() => {
+    if (visible) {
+      input.current.focus();
+      window.addEventListener("mousedown", () => {
+        input.current.blur()
+      })
+    }
+  })
 
   return (
     <>
-      <div>
+      <div style={{
+        position: "relative"
+      }}>
         <input ref={inputref} type="text" value={text} onChange={(e) => { setText(e.target.value) }} />
         <button onClick={handleFocus} >Focus</button>
+        <button onClick={unFocus} >UnFocus</button>
         <div>You typed {text}</div>
-        <div>tou rendered {count.current} {console.log(count.current)} times</div>
-        <div>prev val: {prevValOfTxt.current}</div>const
+        <div>tou rendered {count.current} times</div>
+        <div>prev val: {prevValOfTxt.current}</div>
+        <button onClick={() =>
+          setVisible(true)
+        } >Enter</button>
+        {
+          visible === true ? (
+            <input
+              ref={input}
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "35%"
+              }}
+              value={text}
+              type="text"
+              onChange={(e) => { setText(e.target.value) }}
+            />
+
+          ) :
+            null
+        }
       </div>
     </>
   )
