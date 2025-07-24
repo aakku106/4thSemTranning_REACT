274eb37c95942ae0ce1a0a6bb090268946edea71
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-24 14:30:31 +0545

    useLocation

--- project3/src/Components/Header.jsx
+++ project3/src/Components/Header.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 function Header() {
   let [path, setPath] = useState(window.location.pathname);
+  let location = useLocation()
+  useEffect(() => {
+    setPath(window.location.pathname)
+  }, [location])
   console.log(window.location)
   return (
 
@@ -10,18 +14,15 @@ function Header() {
       <div style={{ display: "flex", width: "20vw", justifyContent: "space-between" }}>
 
         <Link to="/"
-          onClick={() => setPath('/')}
           className={`${path == "/" ? "text-orange-500" : "text-blue-500"}`}
           style={{ fontSize: "20px", textDecoration: "none" }}
         >Home</Link>
         <Link to="/about"
 
-          onClick={() => setPath('/about')}
           className={`${path == "/about" ? "text-orange-500" : "text-blue-500"}`}
           style={{ fontSize: "20px" }}
         >About</Link>
         <Link to="/contact"
-          onClick={() => setPath('/contact')}
           className={`${path == "/contact" ? "text-orange-500" : "text-blue-500"}`}
           style={{ fontSize: "20px" }}
         >Contact</Link>
