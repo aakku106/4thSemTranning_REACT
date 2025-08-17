b3cfd874678c4301c82f957d178db61b1a1017c1
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-08-17 13:25:34 +0545

    .

--- project3/src/Components/SmartComponent/SmartComponent.jsx
+++ project3/src/Components/SmartComponent/SmartComponent.jsx
@@ -6,7 +6,11 @@ import DumbComponent from "../DumbComponent/DumbComponent";
 function SmartComponent() {
   const [text, setText] = useState("Hello ccn, from smart comp!!");
   const changeMessage = () => {
-    setText("Meggage has been changed");
+    setText((prev) =>
+      prev === "Hello ccn, from smart comp!!"
+        ? "Message has been changed!!"
+        : "Hello ccn, from smart comp!!"
+    );
   };
   return (
     <div className=" text-zinc-50-400 p-3.5 bg-blue-500 m-2.5 ">
