f0fac23043631caa454a504b71a2613404c1183c
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-08-20 12:37:47 +0545

    Auto commit + push script ran

--- project3/src/Components/UserList.jsx
+++ project3/src/Components/UserList.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import "./UserList.css";
 import useFetch from "../hooks/useFetch";
 import User from "../Pages/Dashboard/Settings/User";
 
@@ -16,25 +17,35 @@ function UserList() {
       <ul>
         {data.map((user) => (
           <div className="bg-amber-300 p-2 m-2">
-            <li key={user.id}>
+            <li key={user.id - 3}>
               {user.name}, email:{user.email}
             </li>
           </div>
         ))}
       </ul>
-      <table>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-        {data.map((user) => (
+      <table className="bg-blue-500 p-3">
+        <thead>
           <tr>
-            <td>{user.id}</td>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <tr key={user.id - 2} className="p-5">
+              <td key={user.id + 1} className="p-2">
+                {user.id}
+              </td>
+              <td key={user.id + 2} className="p-2">
+                {user.name}
+              </td>
+              <td key={user.id - 1} className="p-2">
+                {user.email}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
