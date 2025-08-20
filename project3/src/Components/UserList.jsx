46653252f1bbf7da7f2d907691cc20e6b08bceb8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-08-20 07:00:26 +0545

    Auto commit + push script ran

--- project3/src/Components/UserList.jsx
+++ project3/src/Components/UserList.jsx
@@ -22,6 +22,20 @@ function UserList() {
           </div>
         ))}
       </ul>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+        {data.map((user) => (
+          <tr>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
