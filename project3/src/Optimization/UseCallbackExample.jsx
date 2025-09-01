7069c85630fa09240df86e7436bfa66b28d6f6f0
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-09-01 13:28:41 +0545

    Auto commit script ran

--- project3/src/Optimization/UseCallbackExample.jsx
+++ project3/src/Optimization/UseCallbackExample.jsx
@@ -6,24 +6,25 @@ import React from "react";
 
 const UseButton = React.memo(({ onClick, children }) => {
   console.log("Rendering button:", children);
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button className="p-2 bg-blue-500 text-white rounded" onClick={onClick}>
+      {children}
+    </button>
+  );
 });
 
 export default function UseCallbackExample() {
-    const [user] = React.useState(["user1", "user2", "user3"]);
-    const handleClick = (name) =>
-    {
-        alert(`Hello, ${name}`);
-    }
+  const [user] = React.useState(["user1", "user2", "user3"]);
+  const handleClick = (name) => {
+    alert(`Hello, ${name}`);
+  };
   return (
     <div>
-          {
-              user.map((name) => (
-                  <UseButton key={name} onClick={() => handleClick(name)}>
-                      {name}
-                  </UseButton>
-              ))
-          }
+      {user.map((name) => (
+        <UseButton key={name} onClick={() => handleClick(name)}>
+          {name}
+        </UseButton>
+      ))}
     </div>
   );
 }
