04d6b423026b0ae2168eac1094505f3439c5a078
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-09-01 13:50:41 +0545

    Auto commit + push script ran

--- project3/src/Optimization/UseCallbackExample.jsx
+++ project3/src/Optimization/UseCallbackExample.jsx
@@ -2,22 +2,24 @@
 
 // memoize the function, protect function to bw re-rendered  each time
 
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 const UseButton = React.memo(({ onClick, children }) => {
   console.log("Rendering button:", children);
   return (
-    <button className="p-2 bg-blue-500 text-white rounded" onClick={onClick}>
+    <button
+      className="p-7 bg-blue-500 text-white rounded m-40"
+      onClick={onClick}>
       {children}
     </button>
   );
 });
 
 export default function UseCallbackExample() {
-  const [user] = React.useState(["user1", "user2", "user3"]);
-  const handleClick = (name) => {
+  const [user] = useState(["user1", "user2", "user3"]);
+  const handleClick = useCallback((name) => {
     alert(`Hello, ${name}`);
-  };
+  }, []);
   return (
     <div>
       {user.map((name) => (
